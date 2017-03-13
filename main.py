b7483c796d0b1cd42a39f485b25d36799fa7dadd
Will Ginsberg <will.j.ginsberg@gmail.com>
2017-03-12 20:40:44 -0400

    Inital commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import re
+
+ANSI_DEFAULT = '\\033[39m'
+
+# Read config
+config_matcher = re.compile(r'^(.*?)(\s*)(\S+)$')
+rules = {}
+with open('config.txt', 'r') as f:
+	for line in f:
+		if len(line) < 3:
+			continue
+		result = config_matcher.match(line)
+		if not result:
+			continue
+		pattern, colour = result.group(1), result.group(3)
+		rules["(%s)" % pattern] = colour
+
+# Do subsitution on stdin
+for line in sys.stdin:
+	subbed = line
+	for pattern, colour in rules.iteritems():
+		subbed = re.sub(pattern, "%s\\1%s" % (colour, ANSI_DEFAULT), subbed)
+	sys.stdout.write(subbed)
